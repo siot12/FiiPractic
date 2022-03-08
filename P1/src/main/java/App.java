b7e54b4a560d03f3b0627bf367428b3786e9ecb1
siot12 <tudorciot@gmail.com>
2022-03-08 16:30:20 +0200

    Initial commit

---
+++ P1/src/main/java/App.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class App {
+    public static void main(String[] args)
+    {
+        System.out.println("Salutare de la Fii!");
+    }
+}
---
+++ P2/src/main/java/App.java
@@ -0,0 +1,7 @@
+
+public class App {
+    public static void main(String[] args)
+    {
+        System.out.println("Salutare de la Fii!");
+    }
+}
